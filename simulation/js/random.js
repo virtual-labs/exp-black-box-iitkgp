aca8d4adb1c1a144c341da10283bb47a4d6d5a9d
vleadadmin <admin@vlabs.ac.in>
2022-12-11 11:26:02 +0000

    https://virtual-labs.github.io/virtual-labs/exp-black-box-iitkgp click on the link to test your code.

---
+++ simulation/js/random.js
@@ -0,0 +1,17 @@
+function randomexp(){
+    var myrandom=Math.round(Math.random()*2)//
+//alert(myrandom);
+ var link1="unkwn2.html"
+    var link2="unkwn.html"
+var link3="unkwn3.html"
+
+    if (myrandom==0)
+        window.open(link1)
+    else if (myrandom==1)
+        window.open(link2)
+ else if (myrandom==2)
+        window.open(link3)
+    
+} 
+
+
